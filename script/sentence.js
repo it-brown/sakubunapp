f7eed6bf1c5ef0faa26969878ffb4dd9c91f519f
derushio <narumi18wa@gmail.com>
2017-03-03 13:22:31 +0900

    パラメータ受取部を実装

--- script/sentence.js
+++ script/sentence.js
@@ -1,1 +1,16 @@
+function getParam() {
+    var arg = {}
+    var pair = decodeURI(location.search.substring(1)).split('&')
+    for (var i = 0; pair[i]; i++) {
+        var kv = pair[i].split('=')
+        arg[kv[0]] = kv[1]
+    }
 
+    return arg
+}
+
+$(function() {
+    // URLに含まれるパラメータを取得
+    var arg = getParam()
+    $("#output").text(arg.selectId + "番目の言葉は" + arg.text + "です")
+})
