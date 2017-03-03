c9ef6f64d805231e21bdd171d58ec044560836a3
derushio <narumi18wa@gmail.com>
2017-03-03 13:03:27 +0900

    add params for html

--- script/index.js
+++ script/index.js
@@ -1,5 +1,10 @@
 $(function() {
-    window.changePage = function(name) {
-        $('#content').attr('src', './page/' + name)
+    window.changePage = function(name, param) {
+        // ページ呼び出しにパラメータを指定できるように
+        var url = './page/' + name
+        if (param != null) {
+            url += "?" + $.param(param)
+        }
+        $('#content').attr('src', url)
     }
 })
